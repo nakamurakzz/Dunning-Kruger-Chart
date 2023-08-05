9776591278ec7d16ecf6b3a96dc7b9c1afdb601a
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-08-05 11:08:26 +0900

    update master

--- master.py
+++ master.py
@@ -10,18 +10,35 @@ if __name__ == "__main__":
     # 補間関数を作成
     f = interp1d(x, y, kind="cubic")
 
-    # 0から6までのx座標を生成
-    xnew = np.linspace(0, 6, 1800)
+    # 0から2までのx座標を生成
+    xnew_1 = np.linspace(0, 2, 900)
 
     # ynewを初期化
-    ynew = f(np.linspace(0, 1, 300))
+    ynew_1 = f(np.linspace(0, 1, 300))
 
     # パターンを繰り返す
-    for i in range(1, 6):
-        start_y = ynew[-1]  # 前の区間の最終値を取得
+    for i in range(1, 2):
+        start_y = ynew_1[-1]  # 前の区間の最終値を取得
         y_segment = f(np.linspace(0, 1, 300)) + \
             (start_y - y[0])  # 各区間を前の区間の終点から始まるように調整
-        ynew = np.concatenate([ynew, y_segment])
+        ynew_1 = np.concatenate([ynew_1, y_segment])
+
+    # 2から6までのx座標を生成
+    xnew_2 = np.linspace(2, 6, 900)
+
+    # ynewを初期化
+    ynew_2 = f(np.linspace(0, 1, 300)) + ynew_1[-1] - y[0]
+
+    # パターンを繰り返す
+    for i in range(1, 4):
+        start_y = ynew_2[-1]  # 前の区間の最終値を取得
+        y_segment = f(np.linspace(0, 1, 300)) * 0.5 + (start_y -
+                                                       y[0] * 0.5)  # 各区間を前の区間の終点から始まるように調整し、yの上昇幅を半分にする
+        ynew_2 = np.concatenate([ynew_2, y_segment])
+
+    # 合体
+    xnew = np.concatenate([xnew_1, xnew_2])
+    ynew = np.concatenate([ynew_1, ynew_2])
 
     # グラフの生成
     plt.figure(figsize=(10, 6))
